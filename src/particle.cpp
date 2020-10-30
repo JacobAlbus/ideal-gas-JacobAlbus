aa4a0474385a7b6ee8ba6315258354c0ccf81d0b
JacobAlbus <70151620+JacobAlbus@users.noreply.github.com>
2020-10-30 13:57:54 -0500

    Implement Particle Constructor

--- src/particle.cpp
+++ src/particle.cpp
@@ -3,7 +3,9 @@
 namespace ideal_gas {
 
 Particle::Particle(glm::vec2 velocity, glm::vec2 position, size_t mass) {
-
+  velocity_ = velocity;
+  position_ = position;
+  kMass_ = mass;
 }
 
 } //namespace ideal_gas
