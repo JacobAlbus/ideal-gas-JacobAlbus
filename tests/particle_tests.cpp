f4e1c8da775b244fc36c95597fd5edf732d1201a
JacobAlbus <70151620+JacobAlbus@users.noreply.github.com>
2020-11-03 16:36:53 -0600

    Update UpdatePosition Tests

--- tests/particle_tests.cpp
+++ tests/particle_tests.cpp
@@ -54,7 +54,7 @@ TEST_CASE("Update Particle Properly Updates Velocity") {
   REQUIRE(particle2.GetVelocity()[1] == Approx(-1.75));
 }
 
-TEST_CASE("Calculate Distance Returns Correct Distance"){
+TEST_CASE("Calculate Distance Returns Correct Distance") {
   glm::vec2 position1(5.0, 6.0);
   glm::vec2 velocity1(0.5, -1.0);
   Particle particle1 = Particle(velocity1, position1, ParticleType::kRed);
@@ -66,16 +66,58 @@ TEST_CASE("Calculate Distance Returns Correct Distance"){
   REQUIRE(particle1.CalculateDistance(particle2) == Approx(1.414213562));
 }
 
-TEST_CASE("Update Position Works Correctly"){
-  glm::vec2 position(5.0, 6.0);
-  glm::vec2 velocity(0.5, -1.0);
-  Particle particle = Particle(velocity, position, ParticleType::kRed);
+TEST_CASE("Update Position Works Correctly") {
+  ci::Rectf gas_window(200.0f, 300.0f, 200.0f, 300.0f);
 
-  ci::Rectf gas_box(200.0f, 300.0f, 200.0f, 300.0f);
+  SECTION("Position updates w/o touching wall") {
+    glm::vec2 position(5.0, 6.0);
+    glm::vec2 velocity(0.5, -1.0);
+    Particle particle = Particle(velocity, position, ParticleType::kRed);
 
-  particle.UpdatePosition(gas_box);
-  REQUIRE(particle.GetPosition()[0] == Approx(5.5));
-  REQUIRE(particle.GetPosition()[1] == Approx(7.0));
+    particle.UpdatePosition(gas_window);
+    REQUIRE(particle.GetPosition()[0] == Approx(5.5));
+    REQUIRE(particle.GetPosition()[1] == Approx(7.0));
+  }
+
+  SECTION("Position Updates After Touching Left Wall") {
+    glm::vec2 position(200.0, 6.0);
+    glm::vec2 velocity(0.5, -1.0);
+    Particle particle = Particle(velocity, position, ParticleType::kRed);
+
+    particle.UpdatePosition(gas_window);
+    REQUIRE(particle.GetPosition()[0] == Approx(199.5));
+    REQUIRE(particle.GetPosition()[1] == Approx(7.0));
+  }
+
+  SECTION("Position Updates After Touching Right Wall") {
+    glm::vec2 position(0.0, 6.0);
+    glm::vec2 velocity(-0.5, -1.0);
+    Particle particle = Particle(velocity, position, ParticleType::kRed);
+
+    particle.UpdatePosition(gas_window);
+    REQUIRE(particle.GetPosition()[0] == Approx(0.5));
+    REQUIRE(particle.GetPosition()[1] == Approx(7.0));
+  }
+
+  SECTION("Position Updates After Touching Top Wall") {
+    glm::vec2 position(100.0, 0.0);
+    glm::vec2 velocity(0.5, -1.0);
+    Particle particle = Particle(velocity, position, ParticleType::kRed);
+
+    particle.UpdatePosition(gas_window);
+    REQUIRE(particle.GetPosition()[0] == Approx(100.5));
+    REQUIRE(particle.GetPosition()[1] == Approx(1.0));
+  }
+
+  SECTION("Position Updates After Touching Bottom Wall") {
+    glm::vec2 position(100.0, 300.0);
+    glm::vec2 velocity(0.5, 1.0);
+    Particle particle = Particle(velocity, position, ParticleType::kRed);
+
+    particle.UpdatePosition(gas_window);
+    REQUIRE(particle.GetPosition()[0] == Approx(100.5));
+    REQUIRE(particle.GetPosition()[1] == Approx(299.0));
+  }
 }
 
 TEST_CASE("IsParticlesInContact Will Allow Velocities To Be Updated") {
@@ -114,7 +156,7 @@ TEST_CASE("IsParticlesMovingCloser Will Allow Velocities To Be Updated") {
   Particle particle2 = Particle(velocity2, position2, ParticleType::kRed);
 
   particle1.UpdateVelocity(particle2);
-  REQUIRE(particle1.GetVelocity() == velocity1);
+  REQUIRE_FALSE(particle1.GetVelocity() == velocity1);
 }
 
 TEST_CASE("IsParticlesMovingCloser Will Stop Velocities From Being Updated") {
@@ -127,5 +169,5 @@ TEST_CASE("IsParticlesMovingCloser Will Stop Velocities From Being Updated") {
   Particle particle2 = Particle(velocity2, position2, ParticleType::kRed);
 
   particle1.UpdateVelocity(particle2);
-  REQUIRE_FALSE(particle1.GetVelocity() == velocity1);
+  REQUIRE(particle1.GetVelocity() == velocity1);
 }
